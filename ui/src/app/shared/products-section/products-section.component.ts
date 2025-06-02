380e1247240e905da684aa66ae4e6d5e93d1273b
nlabrazi <na.labrazi@gmail.com>
2025-06-02 12:58:14 +0200

    fix(style): fix some warn/style for navbar & product-section

--- ui/src/app/shared/products-section/products-section.component.ts
+++ ui/src/app/shared/products-section/products-section.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products-section',
-  imports: [],
-  templateUrl: './products-section.component.html',
-  styleUrl: './products-section.component.scss',
   standalone: true,
+  imports: [CommonModule],
+  templateUrl: './products-section.component.html',
+  styleUrls: ['./products-section.component.scss'],
 })
 export class ProductsSectionComponent {
   products = [
@@ -13,19 +14,19 @@ export class ProductsSectionComponent {
       name: 'Argan Cosmétique',
       description: 'Pour une peau nourrie et des cheveux revitalisés',
       price: 'À partir de 24€',
-      image: '/ui/src/assets/images/asset_1.jpg'
+      image: 'assets/images/asset_1.jpg'
     },
     {
       name: 'Argan Culinaire',
       description: 'L\'huile gourmande aux notes de noisette',
       price: 'À partir de 28€',
-      image: '/ui/src/assets/images/asset_2.jpg'
+      image: 'assets/images/asset_2.jpg'
     },
     {
       name: 'Collection Prestige',
       description: 'L\'huile d\'argan la plus rare et la plus pure',
       price: 'À partir de 42€',
-      image: '/ui/src/assets/images/asset_3.jpg'
+      image: 'assets/images/asset_3.jpg'
     }
   ];
 }
